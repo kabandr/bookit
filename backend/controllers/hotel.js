632320a0075c1a04e80e0690bd1fc7a85df623dc
kabandr <renerugwiro@yahoo.fr>
2023-01-29 23:59:48 +0000

    added Count by City controller

--- backend/controllers/hotel.js
+++ backend/controllers/hotel.js
@@ -44,9 +44,27 @@ export const getHotel = async (req, res, next) => {
 };
 
 export const getAllHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+    }).limit(req.query.limit);
     res.status(200).json(hotels);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
+    res.status(200).json(list);
   } catch (error) {
     next(error);
   }
