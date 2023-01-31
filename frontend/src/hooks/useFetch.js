20f10ddd4458f8b538ed2726acd87883dc66ebdd
Rene Kabanda <renerugwiro@yahoo.fr>
2023-01-31 17:53:57 +0000

    feat: added a react fetching hook.

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useFetch = (url) => {
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true)
+            try {
+                const response = await axios.get(url)
+                setData(response.data)
+            } catch (error) {
+                setError(error)
+            }
+            setLoading(false)
+        }
+        fetchData()
+    }, [url]);
+
+    const reFetch = async () => {
+        setLoading(true)
+        try {
+            const response = await axios.get(url)
+            setData(response.data)
+        } catch (error) {
+            setError(error)
+        }
+        setLoading(false)
+    }
+    return { data, loading, error, reFetch }
+}
+
+export default useFetch
