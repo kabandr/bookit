f1b112ac629987b5c83e4a7d0d4925be5755326d
Rene Kabanda <renerugwiro@yahoo.fr>
2023-01-31 17:57:40 +0000

    chore: updated package.json and .gitignore files.

--- frontend/src/components/featured/Featured.jsx
+++ frontend/src/components/featured/Featured.jsx
@@ -1,6 +1,9 @@
+import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
 const Featured = () => {
+  const {data, loading, error } = useFetch("/hotels/countByCity?cities=berlin,madrid,london")
+
   return (
     <div className="featured">
       <div className="featuredItem">
