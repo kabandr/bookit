f87f66ca0e0b84b40e742728a4078e8074ad05f6
Rene Kabanda <renerugwiro@yahoo.fr>
2023-02-02 00:13:12 +0000

    refactor: refactored PropertyList component.

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">Bookt</span>
+        <span className="logo">Bookits</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
